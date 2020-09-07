a75aa86388b9892ae1b15859976d2623a947e6eb
Josek <Elchin751@gmail.com>
2020-09-07 22:21:58 +0300

    пара фиксов

--- repeat.py
+++ repeat.py
@@ -1,15 +1,18 @@
+import time
 import vk_api
+import traceback
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-#---------------------------------------------------настрой_очка---------------------------------------------------#
+# --------------------------------------------------настрой_очка-------------------------------------------------- #
 token = "токен" # токен VK
 dostup = set([]) # ID тех кто должен быть в доступе, ID цифрами без @id, через запятую, внутри квадратных скобок
 prefix = "!у" # сообщения, начинающиеся с этого префикса, будут расцениваться как команда
 remote_prefix = ".." # префикс для "повторялки"
-#------------------------------------------------------------------------------------------------------------------#
+# ---------------------------------------------------------------------------------------------------------------- #
 
 muted_for_all = set()
 muted_for_me = set()
+
 
 def main():
     session = vk_api.VkApi(token=token)
@@ -19,11 +22,11 @@ def main():
     dostup.add(my_id)
 
     for event in longpoll.listen():
-        def send(text: str):
-            vk.messages.send(peer_id=event.peer_id, random_id=0, message=text)
-
         if event.type != VkEventType.MESSAGE_NEW:
             continue
+
+        def send(text: str):
+            vk.messages.send(peer_id=event.peer_id, random_id=0, message=text)
 
         msg = event.text.lower()
 
@@ -41,9 +44,9 @@ def main():
                 print(f"ЛС: {from_id}: {event.text}")
 
         if from_id in muted_for_all:
-            vk.messages.delete(message_ids=event.message_id,delete_for_all=1)
+            vk.messages.delete(message_ids=event.message_id, delete_for_all=1)
         elif from_id in muted_for_me:
-            vk.messages.delete(message_ids=event.message_id,delete_for_all=0)
+            vk.messages.delete(message_ids=event.message_id, delete_for_all=0)
 
         if from_id in dostup:
             if msg.startswith(prefix):
@@ -65,14 +68,15 @@ def main():
                     dostup.add(user)
                     send(f"[id{user}|Пользователю] выдан доступ.")
                 if msg == "-доступ":
-                    dostup.remove(x)
+                    dostup.remove(user)
                     send(f"[id{user}|Пользователю убран доступ.")
-            elif msg.startswith(remote_prefix):
+            elif msg.startswith(remote_prefix) and from_id != my_id:
                 send(event.text[2:])
-            
+
 
 while True:
-    try: 
+    try:
         main()
-    except: 
-        pass
+    except Exception:
+        print('Ошибка!\n', traceback.format_exc())
+        time.sleep(10)
